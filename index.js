30a5f17f355b451434ba6194ca64854ce8d470e1
sanar4ng <63277972+sanar4ng@users.noreply.github.com>
2021-03-26 15:51:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
-var express = require('express');
-var app = express();
-const cors = require('cors');
-var client_id = 'lmPdtCLlemh9dJhdtQE6'; //api id
-var client_secret = 'DKG7frCgyG';       //api pw
-const http = require("http");
-
-
-app.use(cors());
+const express = require('express');
+const app = express();
+var inputData;
+let users = {}
 var port = process.env.PORT || 3000;
-app.get('/', function (req, res) {
-   var api_url = 'https://openapi.naver.com/v1/search/news?query=' + encodeURI("치매치료"); // json 결과
 
-   var request = require('request');
-   var options = {
-       url: api_url,
-       qs: {start:1,display:30,sort:'sim',query:'치매치료'},
-       headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret,
-       'Access-Control-Allow-Origin': '*',}
-    };
-   request.get(options, function (error, response, body) {
-     if (!error && response.statusCode == 200) {
-       res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-       res.end(body);
-     } else {
-       res.status(response.statusCode).end();
-       console.log('error = ' + response.statusCode);
-     }
+
+app.get('/number', (req, res) => {
+   console.log('who get in here/users');
+   res.json(users)
+});
+
+app.post('/post', (req, res) => {
+   console.log('who get in here post /users');
+   
+
+   req.on('data', (data) => {
+     inputData = JSON.parse(data);
    });
- });
 
- app.listen(port, function () {
-   console.log('http://127.0.0.1:3000/search/news?query=검색어 app listening on port 3000!');
- });
+   req.on('end', () => {
+     console.log("number : "+inputData.number);
+     users.number=inputData.number
+     console.log(users)
+   });
 
- //api server
+   res.write("이동");
+   res.end();
+});
+
+app.listen(port,function () {
+  console.log('Example app listening on port 3000!');
+});
 setInterval(function () {
-  http.get("http://ion-node-js-server.herokuapp.com/");
-}, 600000);
+    http.get("http://ion-node-js-server.herokuapp.com/");
+  }, 600000);
